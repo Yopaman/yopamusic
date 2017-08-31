cb0eada7499b21d8b06055cfaf94dc7d0d3512ef
Yopaman <pablo99@free.fr>
2017-09-01 00:06:51 +0200

    added play command

--- bot.js
+++ bot.js
@@ -2,10 +2,14 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const config = require('./config.json');
 const client = new Discord.Client();
+
+let queue = {
+    urls: [],
+    names: []
+};
+let dispatcher;
+
 client.login(config.bot_token);
-
-let queue = [];
-
 client.on('ready', () => {
     console.log(`Bot connecté en tant que ${client.user.username}`);
 });
@@ -25,16 +29,34 @@ const actions = {
         }
     },
 
-    play(url) {
-
+    play(url, connection, msg, isFirstTime) {
+        if (isFirstTime === true) {queue.urls.push(url)}
+        let stream = ytdl(queue.urls[0]);
+        dispatcher = connection.playStream(stream);
+        dispatcher.on('end', () => {
+            if (queue.urls.length === 1) {
+                queue.urls = [];
+                connection.disconnect();
+            } else {
+                queue.urls.shift();
+                this.play(queue[0], connection, msg);
+            }
+        });
     }
 };
 
 client.on('message', msg => {
     if (msg.channel.type === 'dm') {
-        let args = msg.parse(msg);
-        if (actions.match(msg, play)) {
-
+        let args = actions.parse(msg);
+        if (actions.match(msg, 'play')) {
+            if (queue.urls.length < 1) {
+                client.guilds.get(config.server_id).members.get(msg.author.id).voiceChannel.join()
+                .then(connection => {
+                    actions.play(args[1],connection, msg, true);
+                });
+            } else {
+                queue.urls.push(args[1]);
+            }
         }
     }
 });
